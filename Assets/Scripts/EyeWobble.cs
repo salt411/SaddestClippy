80589116727a44e7448d5ef571553db5c8d11cbd
Adam Breese <adambreese@pm.me>
2024-01-28 01:34:40 -0500

    powerups, speech bubbles, eye wobble
    - updated powerups script
    - added list for speech bubbles
    - added eye wobble script

---
+++ Assets/Scripts/EyeWobble.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeWobble : MonoBehaviour
+{
+    public Transform leftEye;
+    public Transform rightEye;
+    public float wobbleFrequency = 2f; // Adjust the frequency of the wobble
+    public float wobbleAmplitude = 0.02f; // Adjust the amplitude of the wobble
+
+    private Vector3 leftEyeOriginalPosition;
+    private Vector3 rightEyeOriginalPosition;
+
+    void Start()
+    {
+        // Save the original positions of the eyes
+        leftEyeOriginalPosition = leftEye.localPosition;
+        rightEyeOriginalPosition = rightEye.localPosition;
+    }
+
+    void Update()
+    {
+        // Generate random offsets using Perlin noise
+        float time = Time.time * wobbleFrequency;
+        float xOffset = Mathf.PerlinNoise(time, 0f) * wobbleAmplitude;
+        float yOffset = Mathf.PerlinNoise(0f, time) * wobbleAmplitude;
+
+        // Apply the wobble to the eye positions
+        leftEye.localPosition = leftEyeOriginalPosition + new Vector3(xOffset, yOffset, 0f);
+        rightEye.localPosition = rightEyeOriginalPosition + new Vector3(xOffset, yOffset, 0f);
+    }
+}
+
+
--- Assets/Scripts/PowerUpGenerator.cs
+++ Assets/Scripts/PowerUpGenerator.cs
@@ -1,42 +1,53 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpGenerator : MonoBehaviour
 {
     public GameObject keyboardKeyPrefab;
-    public GameObject speechBubblePrefab;
-    public int numberOfPowerUps = 5;
-    public float spawnHeight = 20f;
+    public int maxPowerUpsPerSpawn = 3;
+    public float spawnIntervalMin = 5f;
+    public float spawnIntervalMax = 10f;
     public float minRotationSpeed = 30f;
     public float maxRotationSpeed = 180f;
     public float minMomentum = 5f;
-    public float maxMomentum = 9f;
+    public float maxMomentum = 10f;
 
     void Start()
     {
-        GeneratePowerUps();
+        // Start the coroutine for spawning power-ups
+        StartCoroutine(SpawnPowerUpsCoroutine());
     }
 
-    void GeneratePowerUps()
+    IEnumerator SpawnPowerUpsCoroutine()
     {
-        for (int i = 0; i < numberOfPowerUps; i++)
+        while (true)
         {
-            Vector3 randomPosition = new Vector3(Random.Range(-10f, 10f), spawnHeight, Random.Range(-10f, 10f));
-            GameObject powerUp = Instantiate(keyboardKeyPrefab, randomPosition, Quaternion.identity);
+            // Wait for a random interval before spawning
+            yield return new WaitForSeconds(Random.Range(spawnIntervalMin, spawnIntervalMax));
 
-            // Apply random rotation to the power-up
-            powerUp.transform.rotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
-
-            // Apply random rotation speed
-            float rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
-            powerUp.GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, rotationSpeed, 0f);
-
-            // Apply random momentum
-            float momentum = Random.Range(minMomentum, maxMomentum);
-            powerUp.GetComponent<Rigidbody>().velocity = Vector3.down * momentum;
+            // Spawn a random number of keyboard keys
+            int numPowerUpsToSpawn = Random.Range(1, maxPowerUpsPerSpawn + 1);
+            for (int i = 0; i < numPowerUpsToSpawn; i++)
+            {
+                SpawnKeyboardKey();
+            }
         }
     }
+
+    void SpawnKeyboardKey()
+    {
+        // Spawn a keyboard key at a random position
+        Vector3 randomPosition = new Vector3(Random.Range(-10f, 10f), 15f, -1f);
+        GameObject keyboardKey = Instantiate(keyboardKeyPrefab, randomPosition, Quaternion.identity);
+
+        // Apply random rotation speed and momentum for the keyboard key
+        float rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+        keyboardKey.GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, rotationSpeed, 0f);
+
+        float momentum = Random.Range(minMomentum, maxMomentum);
+        keyboardKey.GetComponent<Rigidbody>().velocity = Vector3.down * momentum;
+    }
 }
+
 
 
