cd0ec9398846c1827d83b623afe9b97e692ea023
Adam Breese <adambreese@pm.me>
2024-01-27 21:52:58 -0500

    keyboard key powerup
    - added keyboard key powerup prefab which will be random colors
    - added random rotation and momentum in powerup generator
    - need to make sure this spawns correctly - probably need ceiling position

--- Assets/Scripts/PowerUpGenerator.cs
+++ Assets/Scripts/PowerUpGenerator.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class PowerUpGenerator : MonoBehaviour
 {
-    public GameObject powerUpPrefab;
+    public GameObject keyboardKeyPrefab;
     public GameObject speechBubblePrefab;
     public int numberOfPowerUps = 5;
-    public string[] sentences = {
-        "Collect me!",
-        "Power up!",
-        "Great job!",
-        "Boost time!",
-        // Add more sentences as needed
-    };
+    public float spawnHeight = 20f;
+    public float minRotationSpeed = 30f;
+    public float maxRotationSpeed = 180f;
+    public float minMomentum = 5f;
+    public float maxMomentum = 10f;
 
     void Start()
     {
@@ -24,9 +22,21 @@ public class PowerUpGenerator : MonoBehaviour
     {
         for (int i = 0; i < numberOfPowerUps; i++)
         {
-            Vector3 randomPosition = new Vector3(Random.Range(-10f, 10f), 20f, Random.Range(-10f, 10f));
-            Instantiate(powerUpPrefab, randomPosition, Quaternion.identity);
+            Vector3 randomPosition = new Vector3(Random.Range(-10f, 10f), spawnHeight, Random.Range(-10f, 10f));
+            GameObject powerUp = Instantiate(keyboardKeyPrefab, randomPosition, Quaternion.identity);
+
+            // Apply random rotation to the power-up
+            powerUp.transform.rotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+
+            // Apply random rotation speed
+            float rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+            powerUp.GetComponent<Rigidbody>().angularVelocity = new Vector3(0f, rotationSpeed, 0f);
+
+            // Apply random momentum
+            float momentum = Random.Range(minMomentum, maxMomentum);
+            powerUp.GetComponent<Rigidbody>().velocity = Vector3.down * momentum;
         }
     }
 }
+
 
